2ec58a5142ffdd253df4f0cce0be0299de2f72c0
Lukasz Gandecki <lgandecki@css.edu>
2017-05-13 16:55:01 +0200

    initial version of chimp 2.0, it works for my meteor tests with multibrowser and calling server

--- bin/chimp.js
+++ bin/chimp.js
@@ -1,4 +1,19 @@
 #!/usr/bin/env node
+var Launcher = require('webdriverio').Launcher;
+var path = require('path');
+var fs   = require('fs');
 
-// be sure to run `npm run prepublish` first to generate the dist dir
-require('../dist/bin/chimp');
+var args = process.argv.slice(2);
+var specs = args.join();
+const configPath = path.join(path.dirname(fs.realpathSync(__filename)), '../wdio.conf.js');
+
+const opts = {
+  spec: specs
+};
+const wdio = new Launcher(configPath, opts);
+
+wdio.run().then(function(code) {
+
+}, function(error) {
+  console.error("failed", error.stacktrace);
+});
---
+++ wdio.conf.js
@@ -0,0 +1,319 @@
+var DDP = require('./src/lib/ddp')
+
+exports.config = {
+
+  // Patterns to exclude.
+  exclude: [
+    // 'path/to/excluded/files'
+  ],
+  //
+  // ============
+  // Capabilities
+  // ============
+  // Define your capabilities here. WebdriverIO can run multiple capabilities at the same
+  // time. Depending on the number of capabilities, WebdriverIO launches several test
+  // sessions. Within your capabilities you can overwrite the spec and exclude options in
+  // order to group specific specs to a specific capability.
+  //
+  // First, you can define how many instances should be started at the same time. Let's
+  // say you have 3 different capabilities (Chrome, Firefox, and Safari) and you have
+  // set maxInstances to 1; wdio will spawn 3 processes. Therefore, if you have 10 spec
+  // files and you set maxInstances to 10, all spec files will get tested at the same time
+  // and 30 processes will get spawned. The property handles how many capabilities
+  // from the same test should run tests.
+  //
+  maxInstances: 10,
+  //
+  // If you have trouble getting all important capabilities together, check out the
+  // Sauce Labs platform configurator - a great tool to configure your capabilities:
+  // https://docs.saucelabs.com/reference/platforms-configurator
+  //
+  capabilities: {
+    browserAlice: {
+      desiredCapabilities: {
+        // maxInstances can get overwritten per capability. So if you have an in-house Selenium
+        // grid with only 5 firefox instances available you can make sure that not more than
+        // 5 instances get started at a time.
+        maxInstances: 1,
+        //
+        browserName: 'chrome'
+      }
+    }, browserBob: {
+        desiredCapabilities: {
+          // maxInstances can get overwritten per capability. So if you have an in-house Selenium
+          // grid with only 5 firefox instances available you can make sure that not more than
+          // 5 instances get started at a time.
+          maxInstances: 1,
+          //
+          browserName: 'chrome'
+        }
+    }
+  },
+
+  //
+  // ===================
+  // Test Configurations
+  // ===================
+  // Define all options that are relevant for the WebdriverIO instance here
+  //
+  // By default WebdriverIO commands are executed in a synchronous way using
+  // the wdio-sync package. If you still want to run your tests in an async way
+  // e.g. using promises you can set the sync option to false.
+  sync: true,
+  //
+  // Level of logging verbosity: silent | verbose | command | data | result | error
+  logLevel: 'result',
+  //
+  // Enables colors for log output.
+  coloredLogs: true,
+  //
+  // If you only want to run your tests until a specific amount of tests have failed use
+  // bail (default is 0 - don't bail, run all tests).
+  bail: 0,
+  //
+  // Saves a screenshot to a given path if a command fails.
+  screenshotPath: './errorShots/',
+  //
+  // Set a base URL in order to shorten url command calls. If your url parameter starts
+  // with "/", then the base url gets prepended.
+  baseUrl: 'http://localhost:3000',
+  //
+  // Default timeout for all waitFor* commands.
+  waitforTimeout: 10000,
+  //
+  // Default timeout in milliseconds for request
+  // if Selenium Grid doesn't send response
+  connectionRetryTimeout: 90000,
+  //
+  // Default request retries count
+  connectionRetryCount: 3,
+  //
+  // Initialize the browser instance with a WebdriverIO plugin. The object should have the
+  // plugin name as key and the desired plugin options as properties. Make sure you have
+  // the plugin installed before running any tests. The following plugins are currently
+  // available:
+  // WebdriverCSS: https://github.com/webdriverio/webdrivercss
+  // WebdriverRTC: https://github.com/webdriverio/webdriverrtc
+  // Browserevent: https://github.com/webdriverio/browserevent
+  // plugins: {
+  //     webdrivercss: {
+  //         screenshotRoot: 'my-shots',
+  //         failedComparisonsRoot: 'diffs',
+  //         misMatchTolerance: 0.05,
+  //         screenWidth: [320,480,640,1024]
+  //     },
+  //     webdriverrtc: {},
+  //     browserevent: {}
+  // },
+  //
+  services: ['selenium-standalone'],
+
+  // Test runner services
+  // Services take over a specific job you don't want to take care of. They enhance
+  // your test setup with almost no effort. Unlike plugins, they don't add new
+  // commands. Instead, they hook themselves up into the test process.
+  // services: [],//
+  // Framework you want to run your specs with.
+  // The following are supported: Mocha, Jasmine, and Cucumber
+  // see also: http://webdriver.io/guide/testrunner/frameworks.html
+  //
+  // Make sure you have the wdio adapter package for the specific framework installed
+  // before running any tests.
+  framework: 'cucumber',
+  //
+  // Test reporter for stdout.
+  // The only one supported by default is 'dot'
+  // see also: http://webdriver.io/guide/testrunner/reporters.html
+  // reporters: ['dot'],//
+  // If you are using Cucumber you need to specify the location of your step definitions.
+  cucumberOpts: {
+    require: [],        // <string[]> (file/dir) require files before executing features
+    backtrace: false,   // <boolean> show full backtrace for errors
+    compiler: [],       // <string[]> ("extension:module") require files with the given EXTENSION after requiring MODULE (repeatable)
+    dryRun: false,      // <boolean> invoke formatters without executing steps
+    failFast: false,    // <boolean> abort the run on first failure
+    format: ['pretty'], // <string[]> (type[:path]) specify the output format, optionally supply PATH to redirect formatter output (repeatable)
+    colors: true,       // <boolean> disable colors in formatter output
+    snippets: true,     // <boolean> hide step definition snippets for pending steps
+    source: true,       // <boolean> hide source uris
+    profile: [],        // <string[]> (name) specify the profile to use
+    strict: false,      // <boolean> fail if there are any undefined or pending steps
+    tags: [],           // <string[]> (expression) only execute the features or scenarios with tags matching the expression
+    timeout: 20000,     // <number> timeout for step definitions
+    ignoreUndefinedDefinitions: false, // <boolean> Enable this config to treat undefined definitions as warnings.
+  },
+
+  //
+  // =====
+  // Hooks
+  // =====
+  // WebdriverIO provides several hooks you can use to interfere with the test process in order to enhance
+  // it and to build services around it. You can either apply a single function or an array of
+  // methods to it. If one of them returns with a promise, WebdriverIO will wait until that promise got
+  // resolved to continue.
+  /**
+   * Gets executed once before all workers get launched.
+   * @param {Object} config wdio configuration object
+   * @param {Array.<Object>} capabilities list of capabilities details
+   */
+  // onPrepare: function (config, capabilities) {
+  // },
+  /**
+   * Gets executed just before initialising the webdriver session and test framework. It allows you
+   * to manipulate configurations depending on the capability or spec.
+   * @param {Object} config wdio configuration object
+   * @param {Array.<Object>} capabilities list of capabilities details
+   * @param {Array.<String>} specs List of spec file paths that are to be run
+   */
+  // beforeSession: function (config, capabilities, specs) {
+  // },
+  /**
+   * Gets executed before test execution begins. At this point you can access to all global
+   * variables like `browser`. It is the perfect place to define custom commands.
+   * @param {Array.<Object>} capabilities list of capabilities details
+   * @param {Array.<String>} specs List of spec file paths that are to be run
+   */
+  before: function (capabilities, specs) {
+    require('babel-register');
+    global.expect = require('xolvio-jasmine-expect').expect;
+    //TODO lgandecki set this up based on the local env, so to not start two browsers if not needed
+    global.browser.instances = [
+      browserAlice, browserBob
+    ];
+    global.server = global.ddp = new DDP('http://127.0.0.1:3000').connect();
+
+
+    var setupDdp = function () {
+      console.log('[chimp][helper] setup DDP');
+        try {
+          console.log('[chimp][helper] connecting via DDP to', process.env['chimp.ddp0']);
+          global.ddp.connectSync();
+          addServerExecute(global.ddp);
+          for(let i = 0; i < global.ddp.instances.length; i++) {
+            console.log('[chimp][helper] connecting via DDP to ' + global.ddp.instances[i]._original.host + ':' + global.ddp.instances[i]._original.port);
+            global.ddp.instances[i].connectSync();
+            addServerExecute(global.ddp.instances[i]);
+          }
+          console.log('[chimp][helper] connecting via DDP had no error');
+        } catch (error) {
+          let errorMessage = error;
+          if (_.isObject(error)) {
+            if (error.code === 'ECONNREFUSED') {
+              log.error('[chimp][helper] Cannot connect to Meteor. Please check if your application is up and running on ' + error.address + ' port ' + error.port);
+            }
+            errorMessage = error.code;
+          }
+          log.error('[chimp][helper] connecting via DDP error', errorMessage);
+          global.browser.endSync();
+          process.exit(1);
+        }
+    };
+
+    var addServerExecute = function (ddpInstance) {
+      ddpInstance.execute = function (func) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        var result;
+        var timeout = parseInt(process.env['chimp.serverExecuteTimeout']) || 10000;
+        setTimeout(function() {
+          if (!result) {
+            throw new Error('[chimp] server.execute timeout after ' + timeout + 'ms');
+          }
+        }, timeout);
+        try {
+          result = ddpInstance.call('xolvio/backdoor', func.toString(), args);
+        } catch (exception) {
+          if (exception.error === 404) {
+            throw new Error('[chimp] You need to install xolvio:backdoor in your meteor app before you can use server.execute()');
+
+          } else {
+            throw exception;
+          }
+        }
+        if (result.error) {
+          var error = new Error('Error in server.execute' + result.error.message);
+          error.stack += '\n' + result.error.stack.replace(/ {4}at/g, '  @');
+          throw error;
+        } else {
+          return result.value;
+        }
+      };
+    };
+    setupDdp();
+  },
+  /**
+   * Hook that gets executed before the suite starts
+   * @param {Object} suite suite details
+   */
+  // beforeSuite: function (suite) {
+  // },
+  /**
+   * Hook that gets executed _before_ a hook within the suite starts (e.g. runs before calling
+   * beforeEach in Mocha)
+   */
+  // beforeHook: function () {
+  // },
+  /**
+   * Hook that gets executed _after_ a hook within the suite starts (e.g. runs after calling
+   * afterEach in Mocha)
+   */
+  // afterHook: function () {
+  // },
+  /**
+   * Function to be executed before a test (in Mocha/Jasmine) or a step (in Cucumber) starts.
+   * @param {Object} test test details
+   */
+  // beforeTest: function (test) {
+  // },
+  /**
+   * Runs before a WebdriverIO command gets executed.
+   * @param {String} commandName hook command name
+   * @param {Array} args arguments that command would receive
+   */
+  // beforeCommand: function (commandName, args) {
+  // },
+  /**
+   * Runs after a WebdriverIO command gets executed
+   * @param {String} commandName hook command name
+   * @param {Array} args arguments that command would receive
+   * @param {Number} result 0 - command success, 1 - command error
+   * @param {Object} error error object if any
+   */
+  // afterCommand: function (commandName, args, result, error) {
+  // },
+  /**
+   * Function to be executed after a test (in Mocha/Jasmine) or a step (in Cucumber) starts.
+   * @param {Object} test test details
+   */
+  // afterTest: function (test) {
+  // },
+  /**
+   * Hook that gets executed after the suite has ended
+   * @param {Object} suite suite details
+   */
+  // afterSuite: function (suite) {
+  // },
+  /**
+   * Gets executed after all tests are done. You still have access to all global variables from
+   * the test.
+   * @param {Number} result 0 - test pass, 1 - test fail
+   * @param {Array.<Object>} capabilities list of capabilities details
+   * @param {Array.<String>} specs List of spec file paths that ran
+   */
+  // after: function (result, capabilities, specs) {
+  // },
+  /**
+   * Gets executed right after terminating the webdriver session.
+   * @param {Object} config wdio configuration object
+   * @param {Array.<Object>} capabilities list of capabilities details
+   * @param {Array.<String>} specs List of spec file paths that ran
+   */
+  // afterSession: function (config, capabilities, specs) {
+  // },
+  /**
+   * Gets executed after all workers got shut down and the process is about to exit. It is not
+   * possible to defer the end of the process using a promise.
+   * @param {Object} exitCode 0 - success, 1 - fail
+   */
+  // onComplete: function(exitCode) {
+  // }
+}
